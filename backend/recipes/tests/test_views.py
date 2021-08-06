66617cfec8e32eb6aac91120a08bda93c402e1b7
DRAGANmik <75855531+DRAGANmik@users.noreply.github.com>
2021-08-06 10:44:37 +0300

    fix tests

--- backend/recipes/tests/test_views.py
+++ backend/recipes/tests/test_views.py
@@ -33,7 +33,7 @@ class ViewRecipeTests(APITestCase):
         super().setUpClass()
 
         UserFactory.create_batch(5)
-        IngredientFactory.create_batch(40)
+        IngredientFactory.create_batch(50)
         RecipeTagFactory.create_batch(4)
         RecipeFactory.create_batch(5)
 
